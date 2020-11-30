975745cea8086859054d660b6640209781221c51
Dima <skadud@mail.ru>
2020-11-30 16:17:45 +0300

    Added homework - №1-7

---
+++ lesson-5.0.5.py
@@ -0,0 +1,13 @@
+# Создать (программно) текстовый файл, записать в него программно набор чисел,
+# разделенных пробелами. Программа должна подсчитывать сумму чисел в файле и выводить ее на экран.
+
+def summary():
+    try:
+        with open('text_5.txt', 'w+') as file_obj:
+            line = input('Введите цифры через пробел \n')
+            file_obj.writelines(line)
+            my_numb = line.split()
+            print(f'Сумма чисел: {sum(map(int, my_numb))}')
+    except ValueError:
+        print('Неправильно набран номер!')
+summary()
